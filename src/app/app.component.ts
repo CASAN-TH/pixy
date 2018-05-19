18d9eff624c45d4b562a6250f3374ed47c5337c9
jigkoh3 <jigkoh3@hotmail.com>
2018-05-19 17:31:15 +0700

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public myInterval: number = 3000;
+  public activeSlideIndex: number = 0;
+  public noWrapSlides: boolean = false;
+
+  activeSlideChange() {
+    console.log(this.activeSlideIndex);
+  }
+
+  public slides: Array<Object> = [
+    { "image": "https://mdbootstrap.com/img/Photos/Slides/img%20(18).jpg" },
+    { "image": "https://mdbootstrap.com/img/Photos/Slides/img%20(19).jpg" },
+    { "image": "https://mdbootstrap.com/img/Photos/Slides/img%20(20).jpg" },
+  ];
 }
