af6a2e39d920982e5dd53f902f29ad3f34f3a1a6
jigkoh3 <jigkoh3@hotmail.com>
2018-05-21 11:00:46 +0700

    update image banner&product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,16 @@ export class AppComponent {
     console.log(this.activeSlideIndex);
   }
 
+  // public slides: Array<Object> = [
+  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/04/Home-Banner_Web_2.jpg" },
+  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/04/Home-Banner_Web_1_V2.jpg" },
+  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/05/BANNER_Promotion_May-01.jpg" },
+  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/05/BANNER_Promotion_May_50_edit-01.jpg"}
+  // ];
   public slides: Array<Object> = [
-    { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/04/Home-Banner_Web_2.jpg" },
-    { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/04/Home-Banner_Web_1_V2.jpg" },
-    { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/05/BANNER_Promotion_May-01.jpg" },
-    { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/05/BANNER_Promotion_May_50_edit-01.jpg"}
+    // { "image": "../assets/images/banner/BANNER_Promotion_1.png" },
+    { "image": "../assets/images/banner/Home-Banner_Web_2.png" },
+    { "image": "../assets/images/banner/Home-Banner_Web_3.png" },
+    { "image": "../assets/images/banner/Home-Banner_Web_4.png"}
   ];
 }
