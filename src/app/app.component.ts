4d6dd27ffb5a35e496e227ee146fe394fe32b133
jigkoh3 <jigkoh3@hotmail.com>
2018-05-30 17:48:29 +0700

    landing page demo #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,16 +15,21 @@ export class AppComponent {
     console.log(this.activeSlideIndex);
   }
 
-  // public slides: Array<Object> = [
-  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/04/Home-Banner_Web_2.jpg" },
-  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/04/Home-Banner_Web_1_V2.jpg" },
-  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/05/BANNER_Promotion_May-01.jpg" },
-  //   { "image": "https://www.lalapixthailand.com/wp-content/uploads/2018/05/BANNER_Promotion_May_50_edit-01.jpg"}
-  // ];
+  
   public slides: Array<Object> = [
-    // { "image": "../assets/images/banner/BANNER_Promotion_1.png" },
     { "image": "../assets/images/banner/Home-Banner_Web_2.png" },
     { "image": "../assets/images/banner/Home-Banner_Web_3.png" },
     { "image": "../assets/images/banner/Home-Banner_Web_4.png"}
+  ];
+
+  public products: Array<Object> = [
+    { "name":"CALENDARS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Calendars.jpg" },
+    { "name":"CARDS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Cards.jpg" },
+    { "name":"DIGITAL OFFSET PRINT","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/DigitalPrints.jpg"},
+    { "name":"FRAMES","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/frames.jpg" },
+    { "name":"GIFTS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Gifts.jpg" },
+    { "name":"PHOTOBOOKS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Photobooks.jpg"},
+    { "name":"PRINTS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Prints.jpg"},
+    { "name":"PROMOTIONS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Promotion.jpg"}
   ];
 }
