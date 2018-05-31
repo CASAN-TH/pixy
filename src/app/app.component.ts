02a0625789ff9bf557b289124026c5064bf8bf1f
jigkoh3 <jigkoh3@hotmail.com>
2018-05-31 07:05:31 +0700

    landing page demo #7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,21 +15,37 @@ export class AppComponent {
     console.log(this.activeSlideIndex);
   }
 
-  
+
   public slides: Array<Object> = [
     { "image": "../assets/images/banner/Home-Banner_Web_2.png" },
     { "image": "../assets/images/banner/Home-Banner_Web_3.png" },
-    { "image": "../assets/images/banner/Home-Banner_Web_4.png"}
+    { "image": "../assets/images/banner/Home-Banner_Web_4.png" }
   ];
 
   public products: Array<Object> = [
-    { "name":"CALENDARS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Calendars.jpg" },
-    { "name":"CARDS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Cards.jpg" },
-    { "name":"DIGITAL OFFSET PRINT","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/DigitalPrints.jpg"},
-    { "name":"FRAMES","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/frames.jpg" },
-    { "name":"GIFTS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Gifts.jpg" },
-    { "name":"PHOTOBOOKS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Photobooks.jpg"},
-    { "name":"PRINTS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Prints.jpg"},
-    { "name":"PROMOTIONS","description":"No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.","image": "../assets/images/products/Promotion.jpg"}
+    { "name": "CALENDARS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Calendars.jpg" },
+    { "name": "CARDS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Cards.jpg" },
+    { "name": "DIGITAL OFFSET PRINT", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/DigitalPrints.jpg" },
+    { "name": "FRAMES", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/frames.jpg" },
+    { "name": "GIFTS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Gifts.jpg" },
+    { "name": "PHOTOBOOKS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Photobooks.jpg" },
+    { "name": "PRINTS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Prints.jpg" },
+    { "name": "PROMOTIONS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Promotion.jpg" }
   ];
+
+  public news: Array<Object> = [
+    {
+      "index": 1, "items": [
+        { "name": "CALENDARS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Calendars.jpg" },
+        { "name": "CARDS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Cards.jpg" },
+        { "name": "DIGITAL OFFSET PRINT", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/DigitalPrints.jpg" }
+      ]
+    },
+    {
+      "index": 2, "items": [
+        { "name": "FRAMES", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/frames.jpg" },
+        { "name": "GIFTS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Gifts.jpg" },
+        { "name": "PHOTOBOOKS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Photobooks.jpg" },
+      ]
+    }];
 }
