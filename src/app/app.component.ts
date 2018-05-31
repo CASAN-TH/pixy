1d4c84095e4d9eff405db777cf885a67e9c66b78
jigkoh3 <jigkoh3@hotmail.com>
2018-05-31 15:49:14 +0700

    zxczxczx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,29 +31,29 @@ export class AppComponent {
   ];
 
   public products: Array<Object> = [
-    { "name": "CALENDARS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Calendars.jpg" },
-    { "name": "CARDS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Cards.jpg" },
-    { "name": "DIGITAL OFFSET PRINT", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/DigitalPrints.jpg" },
-    { "name": "FRAMES", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/frames.jpg" },
-    { "name": "GIFTS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Gifts.jpg" },
-    { "name": "PHOTOBOOKS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Photobooks.jpg" },
-    { "name": "PRINTS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Prints.jpg" },
-    { "name": "PROMOTIONS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/products/Promotion.jpg" }
+    { "name": "CALENDARS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/Calendars.jpg" },
+    { "name": "CARDS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/Cards.jpg" },
+    { "name": "DIGITAL OFFSET PRINT", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/DigitalPrints.jpg" },
+    { "name": "FRAMES", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/frames.jpg" },
+    { "name": "GIFTS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/Gifts.jpg" },
+    { "name": "PHOTOBOOKS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/Photobooks.jpg" },
+    { "name": "PRINTS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/Prints.jpg" },
+    { "name": "PROMOTIONS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/products/Promotion.jpg" }
   ];
 
   public news: Array<Object> = [
     {
       "index": 1, "items": [
-        { "name": "CALENDARS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/whatsnew/whatsnew_1.png" },
-        { "name": "CARDS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/whatsnew/whatsnew_2.png" },
-        { "name": "DIGITAL OFFSET PRINT", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/whatsnew/whatsnew_3.png" }
+        { "name": "CALENDARS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/whatsnew/whatsnew_1.png" },
+        { "name": "CARDS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/whatsnew/whatsnew_2.png" },
+        { "name": "DIGITAL OFFSET PRINT", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/whatsnew/whatsnew_3.png" }
       ]
     },
     {
       "index": 2, "items": [
-        { "name": "FRAMES", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/whatsnew/whatsnew_2.png" },
-        { "name": "GIFTS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/whatsnew/whatsnew_1.png" },
-        { "name": "PHOTOBOOKS", "description": "No photo? No problem! Create unique, custom gifts without photos, too! Choose from plenty of designs to express your style.", "image": "../assets/images/whatsnew/whatsnew_3.png" },
+        { "name": "FRAMES", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/whatsnew/whatsnew_2.png" },
+        { "name": "GIFTS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/whatsnew/whatsnew_1.png" },
+        { "name": "PHOTOBOOKS", "description": "No photo? No problem! Create unique, custom gifts", "image": "../assets/images/whatsnew/whatsnew_3.png" },
       ]
     }];
 
